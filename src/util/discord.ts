5220a3ded1d0ba64177f37787d97cedf6f6c41e3
Alex <alex5402private@gmail.com>
2024-06-09 14:52:12 +0530

    fix rpc

--- src/util/discord.ts
+++ src/util/discord.ts
@@ -2,8 +2,7 @@ class DiscordUser {
     constructor( 
       public username: string, 
       public id: string ,
-      public discriminator: string,
     ) {}
   }
   
-  export let user = new DiscordUser("alex5402", '803060151082418217', "hi");
+  export let user = new DiscordUser("alex5402", '803060151082418217');
